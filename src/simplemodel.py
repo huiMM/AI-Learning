c5ca2362b308f8e3a5ba67b40818723548be0eb5
xusheng <klein203@hotmail.com>
2018-03-12 18:44:43 +0800

    20180312

--- src/simplemodel.py
+++ src/simplemodel.py
@@ -2,94 +2,152 @@
 @author: xusheng
 '''
 
+import argparse
+import os
+import sys
+import re
+
+# from six.moves import xrange
 import tensorflow as tf
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn import datasets
-from six.moves import xrange
-from _testbuffer import ndarray
+# import numpy as np
+from tensorflow.examples.tutorials.mnist import input_data
+from tensorflow.python.training.basic_session_run_hooks import StepCounterHook
+
+FLAGS = None
+
+IMG_SIZE = 28
+
+    
+def main(_):
+    # raw data
+    dataset = input_data.read_data_sets(FLAGS.input_data_dir, FLAGS.fake_data)
+
+    # batch preparation
+    ds_training = tf.data.Dataset.from_tensor_slices((dataset.train.images, dataset.train.labels))
+    ds_training = ds_training.shuffle(buffer_size=10000)
+    ds_training = ds_training.batch(FLAGS.batch_size)
+    ds_training = ds_training.repeat(FLAGS.max_epoches)
+    iter = ds_training.make_one_shot_iterator()
+
+    # logits
+    x_data, y_label = iter.get_next()
+    
+    # [BATCH, 28, 28] - reshape [BATCH, 28, 28, 1]
+    input = tf.reshape(x_data, shape=[FLAGS.batch_size, IMG_SIZE, IMG_SIZE, 1])
+    
+    with tf.variable_scope('conv1'):
+        # [BATCH, 28, 28, 1] - conv2d(k=2, s=2, o=64), relu [BATCH, 14, 14, 1]
+        # parameters: weight [2, 2, 1, 64], bias [64]
+        kernel = tf.get_variable(name='weight', dtype=tf.float32, shape=[2, 2, 1, 64], initializer=tf.truncated_normal_initializer(mean=0., stddev=1.))
+        bias = tf.get_variable(name='bias', dtype=tf.float32, shape=[64], initializer=tf.constant_initializer(0., dtype=tf.float32))
+        conv = tf.nn.conv2d(input, filter=kernel, strides=[1, 2, 2, 1], padding='SAME')
+        logits = tf.nn.bias_add(conv, bias)
+        logits = tf.nn.relu(logits)
+        
+    with tf.variable_scope('pool1'):
+        # [BATCH, 14, 14, 1] - maxpool(k=2, s=2) [BATCH, 7, 7, 1]
+        logits = tf.nn.max_pool(logits, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+    with tf.variable_scope('fc1'):
+        # [BATCH, 7, 7, 1] - reshape [BATCH, 64*7*7] - linear, relu [BATCH, 256]
+        # parameters: weight [64*7*7, 256], bias [256]
+        logits = tf.reshape(logits, shape=[-1, 64*7*7])
+        weight = tf.get_variable(name='weight', dtype=tf.float32, shape=[64*7*7, 256], initializer=tf.truncated_normal_initializer(mean=0., stddev=1.))
+        bias = tf.get_variable(name='bias', dtype=tf.float32, shape=[256], initializer=tf.constant_initializer(0., dtype=tf.float32))
+        logits = tf.nn.bias_add(tf.matmul(logits, weight), bias)
+        logits = tf.nn.relu(logits)
+
+    with tf.variable_scope('fc2'):
+        # [BATCH, 256] - linear, relu [BATCH, 10]
+        # parameters: weight [256, 10], bias [10]
+        weight = tf.get_variable(name='weight', dtype=tf.float32, shape=[256, 10], initializer=tf.truncated_normal_initializer(mean=0., stddev=1.))
+        bias = tf.get_variable(name='bias', dtype=tf.float32, shape=[10], initializer=tf.constant_initializer(0., dtype=tf.float32))
+        logits = tf.nn.bias_add(tf.matmul(logits, weight), bias)
+    
+    # loss: cross entropy
+    loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=tf.cast(y_label, tf.int32), logits=logits))
+    
+    # optimizer
+    training_op = tf.train.GradientDescentOptimizer(FLAGS.learning_rate).minimize(loss)
+
+    # initializer
+    init_op = tf.global_variables_initializer()
+    
+    # ckpt saver
+    saver = tf.train.Saver()
+    
+    model_name = "mnist-alpha.model"
+    checkpoint_dir = os.path.join(FLAGS.log_dir, model_name)
+    
+    # run session
+    with tf.train.MonitoredTrainingSession(checkpoint_dir=checkpoint_dir, 
+                                           hooks=[tf.train.StopAtStepHook(last_step=FLAGS.max_epoches*55000/FLAGS.batch_size/2)] 
+#                                            save_checkpoint_secs=120, 
+#                                            save_summaries_steps=100
+                                           ) as mon_sess:
+        print("0")
+        mon_sess.run(init_op)
+#         sess.run(tf.local_variables_initializer())
+        
+        global_step = 0
+        
+        # ckpt saver restore
+        ckpt = tf.train.get_checkpoint_state(checkpoint_dir)
+        if ckpt and ckpt.model_checkpoint_path:
+            print("1")
+            ckpt_name = os.path.basename(ckpt.model_checkpoint_path)
+            saver.restore(mon_sess, os.path.join(checkpoint_dir, ckpt_name))
+            global_step = int(next(re.finditer("(\d+)(?!.*\d)", ckpt_name)).group(0))
+            print("Success to read %s, step starts from %d" % (ckpt_name, global_step))
+        else:
+            print("2")
+            if not os.path.exists(checkpoint_dir):
+                os.makedirs(checkpoint_dir)           
+            
+        while not mon_sess.should_stop():
+            print("3")
+            mon_sess.run(training_op)
+            print("Step %d, loss = %s" % (global_step, mon_sess.run(loss)))        
+
 
 if __name__ == '__main__':
-    
-#     x_vals = np.random.normal(1, 0.1, 100)
-#     y_vals = np.repeat(10., 100)
-#     
-#     x_data = tf.placeholder(tf.float32, shape=[None, 1])
-#     y_target = tf.placeholder(tf.float32, shape=[None, 1])
-#     
-#     A = tf.Variable(tf.random_normal([1, 1]))
-#     
-#     my_output = tf.matmul(x_data, A)
-#     
-#     loss = tf.nn.l2_loss(my_output - y_target)
-# 
-#     opt = tf.train.GradientDescentOptimizer(0.02)
-#     
-#     train_step = opt.minimize(loss)
-#     
-#     init = tf.global_variables_initializer()
-#     with tf.Session() as sess:
-#         sess.run(init)
-#         
-#         loss_batch = []
-#         for i in xrange(100):
-#             rand_index = np.random.choice(100, size=BATCH_SIZE)
-#             rand_x = np.transpose([x_vals[rand_index]])
-#             rand_y = np.transpose([y_vals[rand_index]])
-#             sess.run(train_step, feed_dict={x_data: rand_x, y_target: rand_y})
-#             if (i+1)%1 == 0:
-#                 print('Step #%d: A = %s' % ((i + 1), sess.run(A)))
-#                 loss_eval = sess.run(loss, feed_dict={x_data:rand_x, y_target: rand_y})
-#                 print('Loss = %s' % loss_eval)
-#                 loss_batch.append(loss_eval)
-# 
-#         plt.plot(range(0, 100, 1), loss_batch, 'r-', label='Batch Loss, size=20')
-#         plt.legend(loc='upper right', prop={'size': 11})
-#         plt.show()
-    
-    BATCH_SIZE = 20
-    EPOCHES = 1000
-    
-    iris = datasets.load_iris()
-#     binary_target = np.array([1. if x == 0 else 0. for x in iris.target])
-    x_data = tf.placeholder(shape=[None, 1], dtype=tf.float32)
-    y_target = tf.placeholder(shape=[None, 1], dtype=tf.float32)
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+            '--learning_rate',
+            type=float,
+            default=1e-3,
+            help='Initial learning rate.'
+    )
+    parser.add_argument(
+            '--max_epoches',
+            type=int,
+            default=2,
+            help='Number of epoches to run trainer.'
+    )
+    parser.add_argument(
+            '--batch_size',
+            type=int,
+            default=100,
+            help='Batch size.    Must divide evenly into the dataset sizes.'
+    )
+    parser.add_argument(
+            '--input_data_dir',
+            type=str,
+            default=os.path.join(os.getenv('TEST_TMPDIR', '..\\'), 'data\\mnist'),
+            help='Directory to put the input data.'
+    )
+    parser.add_argument(
+            '--log_dir',
+            type=str,
+            default=os.path.join(os.getenv('TEST_TMPDIR', '..\\'), 'logs'),
+            help='Directory to put the log data.'
+    )
+    parser.add_argument(
+            '--fake_data',
+            default=False,
+            help='If true, uses fake data for unit testing.',
+            action='store_true'
+    )
 
-    a = tf.Variable(tf.random_normal(shape=[1, 1]))
-    b = tf.Variable(tf.random_normal(shape=[1]))
-
-    inference = tf.add(tf.matmul(x_data, a), b)
-
-    loss = tf.nn.l2_loss(inference - y_target)
-
-    my_opt = tf.train.GradientDescentOptimizer(0.02)
-    train_step = my_opt.minimize(loss)
-
-    init = tf.global_variables_initializer()
-    with tf.Session() as sess:
-        sess.run(init)
-
-        # Training
-        loss_batch = []
-        for i in xrange(EPOCHES):
-            rand_index = np.random.choice(len(iris.data), size=BATCH_SIZE)
-            
-            rand_x = np.reshape(iris.data[rand_index, 3], [BATCH_SIZE, 1])
-            rand_y = np.reshape(iris.data[rand_index, 0], [BATCH_SIZE, 1])
-            
-            sess.run(train_step, feed_dict={x_data: rand_x, y_target: rand_y})
-            if (i+1)%1==0:
-                l = sess.run(loss, feed_dict={x_data: rand_x, y_target: rand_y})
-                loss_batch.append(l)
-#                 print('Step #%d， A = %s, b = %s, loss = %s' % ((i+1), sess.run(a), sess.run(b), l))
-    
-        # Testing
-
-        # Diagram        
-        plt.plot(range(0, EPOCHES, 1), loss_batch, 'r-', label='L2 loss per epoches')
-        plt.legend(loc='upper right', prop={'size': 11})
-        plt.show()
-
-
-
-    
+    FLAGS, unparsed = parser.parse_known_args()
+    tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
--- src/test.py
+++ src/test.py
@@ -1,10 +1,73 @@
 '''
 @author: xusheng
 '''
-import numpy as np 
+
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from six.moves import xrange
 
 if __name__ == '__main__':
-    x_val = np.linspace(-5, 5, 100)
-    y_val = np.random.normal(0, 1)
+    BATCH_SIZE = 20
+    EPOCHES = 20000
+    NUM_CAT = 3
+    
+    iris = datasets.load_iris()
+#     binary_target = np.array([1. if x == 0 else 0. for x in iris.target])
+    x_data = tf.placeholder(shape=[None, 4], dtype=tf.float32)
+    y_target = tf.placeholder(shape=[None], dtype=tf.int32)
+
+    a = tf.Variable(tf.random_normal(shape=[4, NUM_CAT]))
+    b = tf.Variable(tf.random_normal(shape=[NUM_CAT]))
+
+    inference = tf.reshape(tf.add(tf.matmul(x_data, a), b), [BATCH_SIZE, NUM_CAT])
+
+#     alpha = 1.
+#     lamb = 1.
+#     a_l1_loss = tf.reduce_mean(tf.abs(a))
+#     a_l2_loss = tf.reduce_mean(tf.square(a))
+#     loss = tf.nn.l2_loss(inference - y_target) + lamb * ((1-alpha) * a_l2_loss + alpha * a_l1_loss) 
+    loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y_target, logits=inference))
+    
+    my_opt = tf.train.GradientDescentOptimizer(1e-3)
+    train_step = my_opt.minimize(loss)
+
+    init = tf.global_variables_initializer()
+    with tf.Session() as sess:
+        sess.run(init)
+
+        # Training
+        total_loss_batch = []
+#         a_l1_loss_batch = []
+#         a_l2_loss_batch = []
+        for i in xrange(EPOCHES):
+            rand_index = np.random.choice(len(iris.data), size=BATCH_SIZE)
+            
+            rand_x = np.reshape(iris.data[rand_index, :], [BATCH_SIZE, 4])
+            rand_y = iris.target[rand_index]
+            
+            sess.run(train_step, feed_dict={x_data: rand_x, y_target: rand_y})
+            
+            epoch_loss = sess.run(loss, feed_dict={x_data: rand_x, y_target: rand_y})
+            total_loss_batch.append(epoch_loss)
+            if (i+1)%100==0:
+#                 a_l1_l = sess.run(a_l1_loss, feed_dict={x_data:rand_x})
+#                 a_l1_loss_batch.append(a_l1_l)
+#                 a_l2_l = sess.run(a_l2_loss, feed_dict={x_data:rand_x})
+#                 a_l2_loss_batch.append(a_l2_l)
+                print('Step #%d， A = %s, b = %s, loss = %s' % ((i+1), sess.run(a), sess.run(b), epoch_loss))
+#                 print('Step #%d， loss = %s' % ((i+1), l))
+    
+        # Testing
+
+        # Diagram
+        plt.plot(range(0, EPOCHES, 1), total_loss_batch, 'b-', label='Total Loss')
+#         plt.plot(range(0, EPOCHES, 1), a_l1_loss_batch, 'b--', label='Alaph loss l1 per epoches')
+#         plt.plot(range(0, EPOCHES, 1), a_l2_loss_batch, 'g--', label='Alaph loss l2 per epoches')
+        plt.legend(loc='upper right', prop={'size': 10})
+        plt.show()
+
+
 
     
