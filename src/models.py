464edcae4e3d56af4acd6cf0b750da2f9df12e2e
xusheng <klein203@hotmail.com>
2018-08-16 18:29:39 +0800

    load_weights bugfix

--- src/models.py
+++ src/models.py
@@ -6,6 +6,8 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Activation, Conv2D, MaxPool2D, Flatten, Reshape
 import os
 
+import numpy as np
+
 class Model(object):
     def __init__(self, name, load_weights=False, **kwargs):
         self._name = name
@@ -51,9 +53,9 @@ class Model(object):
     
     def build_model(self):
         model = self._build_model()
-        if self._load_weights:
+        if self._load_weights == True:
             model.load_weights(os.path.join(self._log_path, self._h5py))
-        
+        model.summary()
         self._model = model
         return self._model
     
@@ -82,5 +84,35 @@ class LeNet5(Model):
         
         model.add(Dense(self._classes))
         model.add(Activation('softmax'))
+        
+#         for layer in model.layers:
+#             weight = layer.get_weights()
+#             print(layer, np.asarray(weight).shape)
         return model
-    
+
+# class DCGAN(Model):
+#     def __init__(self, name='DCGAN', load_weights=False, **kwargs):
+#         super(DCGAN, self).__init__(name=name, load_weights=load_weights, **kwargs)
+# 
+#     def _build_model(self):
+#         model = Sequential()
+#         if self._input_shape is None:
+#             model.add(Reshape(target_shape=(self._row, self._col, self._channel), input_shape=(self._row * self._col * self._channel,)))
+#         else:
+#             # TODO: _input_shape as input
+#             pass
+#         model.add(Conv2D(filters=6, kernel_size=5, padding='same'))
+#         model.add(Activation('relu'))
+#         model.add(MaxPool2D(pool_size=2, strides=2))
+#         
+#         model.add(Conv2D(filters=16, kernel_size=5, padding='same'))
+#         model.add(Activation('relu'))
+#         model.add(MaxPool2D(pool_size=2, strides=2))
+#         
+#         model.add(Flatten())
+#         model.add(Dense(84))
+#         model.add(Activation('relu'))
+#         
+#         model.add(Dense(self._classes))
+#         model.add(Activation('softmax'))
+#         return model
